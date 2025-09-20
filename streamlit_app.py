cd052b0900442dc4c2214ca9a1d763d66cd012e2
khyatinagaria <146091358+khyatinagaria@users.noreply.github.com>
2025-09-20 18:27:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
-
+import streamlit as st
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 st.title('😶‍🌫️ machine learning app')
 
-st.write('this is app builds a machine learning model!')
+st.info('This is app builds a machine learning model!')
+with st.expander('Data'):
+  df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+  df
+  st.write('**y**')
+  y_raw = df.species
+  y_raw
