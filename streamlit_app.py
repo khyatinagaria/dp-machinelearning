c8a23462bb33b986b3cc28e123eb2f2fed1c817f
khyatinagaria <146091358+khyatinagaria@users.noreply.github.com>
2025-09-20 18:28:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,9 @@ st.info('This is app builds a machine learning model!')
 with st.expander('Data'):
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
   df
+  st.write('**x**')
+  x_raw = df.drop('species',axis=1)
+  x_raw
   st.write('**y**')
   y_raw = df.species
   y_raw
